4e1ec5e3cd423f59469d54c3243beed909c23ca0
Nurul Chotimah <46445897+nurulle@users.noreply.github.com>
2020-03-19 16:26:16 +0700

    Add files via upload
    Tugas 1

---
+++ OOP1/src/Tugas1/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1;
+
+/**
+ *
+ * @author Choco Calon Presiden RI
+ */
+public class Mahasiswa {
+    public static void main(String[] args) {
+        String nim = "18090046";
+        String nama = "Nurul Chotimah";
+        String kelas = "4C";
+       
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+    
+}
